72c00fdad2b24a51792736e71684e706b4b7ddd9
Neal Stephenson <neal@bakerst.org>
2025-06-28 07:41:59 -0400

    removing clutter to examples

--- lib/common/base.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-#include <ArduinoLog.h>
-
-#include "sutils.h"
--- lib/sutils/sutils.h
+++
@@ -1,9 +0,0 @@
-#ifndef SUTILS_H
-#define SUTILS_H
-
-#include <Arduino.h>
-
-bool safe_str_copy(char *dest, const char *src, size_t size);
-const char *splitString(const char *input, char split=' ');
-
-#endif  // SUTILS_H
